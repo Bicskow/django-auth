74262497040bb306b51d371f1135edaeb85bc5fb
bicskow <bicskow@gmail.com>
2026-05-16 22:45:54 +0200

    basic login/logout implemented

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
+
+def login_view(request: HttpRequest):
+    error = None
+
+    if request.method == "POST":
+        user = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=user, password=password)
+
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = "Invalid username or password"
+
+    return render(request, "login.html", {"error": error})
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
