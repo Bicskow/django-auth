c160edccbab443819bf5d4ac929aac60e3cb3f46
bicskow <bicskow@gmail.com>
2026-06-23 16:07:25 +0200

    psw reset templates added for allauth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,28 +1,6 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
-    path(
-        "password-reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="password_reset",
-    ),
-    path(
-        "password-reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
 ]
